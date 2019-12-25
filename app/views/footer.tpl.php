3681f05fb550aa2db55e4b1cdc85f869b1e4a27b
py eye <pyd3.14@gmail.com>
2019-12-25 18:37:42 +0100

    Amélioration du responsive

--- app/views/footer.tpl.php
+++ app/views/footer.tpl.php
@@ -2,7 +2,8 @@
     <span>&copy;</span>
     <div class="logo">
 
-        <img src="<?= $absoluteUrl ?>/assets/images/logo-alpha.png" alt="logo">
+        pyd
+        <!--<img src="<?= $absoluteUrl ?>/assets/images/logo-alpha.png" alt="logo">-->
 
     </div>
     <span>2019-2020</span>
